476ad57d78df6e90975c743a92d1d96dba68dc8e
gumrahsindar <gsindar@gmail.com>
2024-08-26 20:49:26 +0300

    fead: empty job page for initial rendering

---
+++ src/components/EmptyJobContent.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="job-details">
+    <div>
+      <div class="job-details__start-view">
+        <p>What are you looking for?</p>
+        <p>
+          Start by searching for any technology your ideal job is working with
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
