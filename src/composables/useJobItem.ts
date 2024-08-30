3bbd5423cfbc2631d786b6c8816fd9f0ef824f60
gumrahsindar <gsindar@gmail.com>
2024-08-30 15:54:35 +0300

    refactor: Refactor useJobItem and useJobItems composables with Tanstack Query

--- src/composables/useJobItem.ts
+++ src/composables/useJobItem.ts
@@ -1,29 +1,36 @@
-import { ref, watch, Ref } from "vue"
+import { computed, Ref } from "vue"
 import { JobItemExtended } from "../types"
+import { useQuery } from "@tanstack/vue-query"
+
+type TJobItemApiResponse = {
+  public: boolean
+  jobItem: JobItemExtended
+}
+
+const fetchJobItem = async (id: number | null): Promise<JobItemExtended> => {
+  const res = await fetch(`${import.meta.env.VITE_BASE_API_URL}/${id}`)
+  if (!res.ok) {
+    const errorData = await res.json()
+    console.error(errorData.description)
+    throw new Error(errorData.description)
+  }
+  const data: TJobItemApiResponse = await res.json()
+  return data.jobItem
+}
 
 export function useJobItem(id: Ref<number | null>) {
-  const jobItem = ref<null | JobItemExtended>(null)
-  const isLoading = ref(false)
+  const jobId = computed(() => id.value)
+  const enabled = computed(() => !!jobId.value)
+  const {
+    data: jobItem,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["job", jobId], // Use id.value here to ensure stability
+    queryFn: () => fetchJobItem(jobId.value),
+    enabled,
+    staleTime: 1000 * 60 * 60, // 1 hour
+  })
 
-  const fetchData = async () => {
-    if (!id.value) return
-
-    isLoading.value = true
-    try {
-      const res = await fetch(
-        `${import.meta.env.VITE_BASE_API_URL}/${id.value}`
-      )
-      const data = await res.json()
-      console.log(data.jobItem)
-      jobItem.value = data.jobItem
-    } catch (error) {
-      console.error(error)
-    } finally {
-      isLoading.value = false
-    }
-  }
-
-  watch(id, fetchData)
-
-  return { jobItem, isLoading }
+  return { jobItem, isLoading, error }
 }
--- src/composables/useJobItems.ts
+++ src/composables/useJobItems.ts
@@ -1,34 +1,52 @@
-import { computed, ref, watchEffect } from "vue"
+import { computed, Ref, watch, watchEffect } from "vue"
 import { JobItem } from "../types"
+import { useQuery } from "@tanstack/vue-query"
+import { toast, type ToastOptions } from "vue3-toastify"
 
-export function useJobItems(searchText: { value: string }) {
-  const jobItems = ref<JobItem[]>([])
-  const isLoading = ref(false)
+type TJobItemsApiResponse = {
+  jobItems: JobItem[]
+  public: boolean
+  sorted: boolean
+}
 
-  watchEffect(async () => {
-    if (!searchText.value) return
+type TSearchText = Ref<string | unknown>
 
-    const fetchData = async () => {
-      isLoading.value = true
-      try {
-        const res = await fetch(
-          `${import.meta.env.VITE_BASE_API_URL}?search=${searchText.value}`
-        )
-        const data = await res.json()
-        jobItems.value = data.jobItems
-      } catch (error) {
-        console.error(error)
-      } finally {
-        isLoading.value = false
+const fetchJobItems = async (
+  searchText: TSearchText
+): Promise<JobItem[] | undefined> => {
+  const res = await fetch(
+    `${import.meta.env.VITE_BASE_API_URL}?search=${searchText.value}`
+  )
+  if (!res.ok) {
+    const errorData = await res.json()
+    throw new Error(errorData.description)
+  }
+  const data: TJobItemsApiResponse = await res.json()
+  return data.jobItems
+}
+
+export function useJobItems(searchText: TSearchText) {
+  const searchTextKey = computed(() => searchText.value)
+  const enabled = computed(() => !!searchText.value)
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["jobItems", searchTextKey],
+    queryFn: () => fetchJobItems(searchText),
+    staleTime: 1000 * 60 * 60, // 1 hour
+    enabled,
+    retry: 1,
+  })
+
+  watch(
+    error,
+    (newError) => {
+      if (newError) {
+        toast(newError.message, {
+          position: toast.POSITION.BOTTOM_RIGHT,
+        } as ToastOptions)
       }
-    }
+    },
+    { immediate: true }
+  )
 
-    fetchData()
-  })
-
-  const JobItemsSliced = computed(() => {
-    return jobItems.value.slice(0, 7)
-  })
-
-  return { jobItems: JobItemsSliced, isLoading }
+  return { jobItems: data, isLoading, error }
 }
