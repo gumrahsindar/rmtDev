e0c9b8729499adce5eda2309902660cffa43364d
gumrahsindar <gsindar@gmail.com>
2024-08-30 15:52:41 +0300

    chore: Update npm dependencies and add new dependencies for VueQueryPlugin and Vue3Toastify

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { VueQueryPlugin } from "@tanstack/vue-query"
+import { createApp } from "vue"
+import "./style.css"
+import App from "./App.vue"
+import Vue3Toastify, { type ToastContainerOptions } from "vue3-toastify"
+Vue3Toastify
+import "vue3-toastify/dist/index.css"
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(VueQueryPlugin)
+
+app.use(Vue3Toastify, {
+  autoClose: 3000,
+} as ToastContainerOptions)
+
+app.mount("#app")
