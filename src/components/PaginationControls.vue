7fc6f0d58de4da3e2ddece8ac9c807a93605b443
gumrahsindar <gsindar@gmail.com>
2024-08-30 15:55:11 +0300

    feat: Add pagination functionality to PaginationControls.vue

--- src/components/PaginationControls.vue
+++ src/components/PaginationControls.vue
@@ -1,7 +1,79 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps<{
+  totalPages: number
+}>()
+const currentPage = defineModel("currentPage", {
+  type: Number,
+  default: 1,
+})
+
+const handlePageChange = (direction: "next" | "previous") => {
+  if (direction === "next" && currentPage.value) {
+    currentPage.value++
+  } else if (direction === "previous" && currentPage.value) {
+    currentPage.value--
+  }
+}
+</script>
 
 <template>
-  <section class="pagination"></section>
+  <section class="pagination">
+    <button
+      v-if="currentPage > 1"
+      class="pagination__button"
+      @click="
+        (e) => {
+          handlePageChange('previous')
+          const target = e.currentTarget as HTMLButtonElement
+          target.blur()
+        }
+      "
+    >
+      <svg
+        width="15"
+        height="15"
+        viewBox="0 0 15 15"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M6.85355 3.14645C7.04882 3.34171 7.04882 3.65829 6.85355 3.85355L3.70711 7H12.5C12.7761 7 13 7.22386 13 7.5C13 7.77614 12.7761 8 12.5 8H3.70711L6.85355 11.1464C7.04882 11.3417 7.04882 11.6583 6.85355 11.8536C6.65829 12.0488 6.34171 12.0488 6.14645 11.8536L2.14645 7.85355C1.95118 7.65829 1.95118 7.34171 2.14645 7.14645L6.14645 3.14645C6.34171 2.95118 6.65829 2.95118 6.85355 3.14645Z"
+          fill="currentColor"
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+      Page {{ currentPage - 1 }}
+    </button>
+    <button
+      v-if="currentPage < totalPages"
+      class="pagination__button"
+      :class="{ 'pagination__button--next': currentPage === 1 }"
+      @click="
+        (e) => {
+          handlePageChange('next')
+          const target = e.currentTarget as HTMLButtonElement
+          target.blur()
+        }
+      "
+    >
+      Page {{ currentPage + 1 }}
+      <svg
+        width="15"
+        height="15"
+        viewBox="0 0 15 15"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
+          fill="currentColor"
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+    </button>
+  </section>
 </template>
 
 <style scoped></style>
